a35801a80e68f02d5ef42bc46445eafc07901bd6
Veresik <75386770+Veresik@users.noreply.github.com>
2021-04-07 22:59:52 +0200

    Dodanie oraz edycja bootstrapa

---
+++ resources/views/home.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.admin')
+
+@section('content')
+<!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Scamanor</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Strona główna</a></li>
+              
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>1 </h3>
+                <p> 2 </p>
+              </div>
+              <div class="icon">
+              
+              </div>
+              <a href="payin" class="small-box-footer">3 <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>3<sup style="font-size: 20px"></sup></h3>
+                <p> 4 <p>
+              </div>
+              <div class="icon">
+                
+              </div>
+              <a href="payout" class="small-box-footer">5 <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>6 </h3>
+
+                <p>7 </p>
+              </div>
+              <div class="icon">
+         
+              </div>
+              <a href="report" class="small-box-footer">8<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+         <!-- ./col -->
+         <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>9</h3>
+
+                <p>10 </p>
+              </div>
+              <div class="icon">
+              
+              </div>
+              <a href="reporthis" class="small-box-footer">11<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+               
+
+              
+
+           
+           
+
+            
+               
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </section>
+          <!-- right col -->
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
